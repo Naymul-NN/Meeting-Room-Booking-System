be5f42f6de6696785ab77388f948ccf21d5691f2
Naymul-NN <naymulislam108@gmail.com>
2024-06-20 16:02:13 +0600

    validation: zod

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
-// import validationRequest from '../middleware/validateRequest';
-// import { UserValidation } from './user.validation';
+import validationRequest from '../middleware/validateRequest';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
 router.post('/signup',
-    // validationRequest(UserValidation.userVlidationSchema) ,
+    validationRequest(UserValidation.userValidationSchema) ,
     UserControllers.userSingup);
 
 export const UserRoutes = router
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 import { Role } from "./user.constant";
 
-const userVlidationSchema = z.object({
-    
-    password: z.string().max(20, {message: 'password can not be more then 20 characters'}).optional(),
-    role: z.enum([...Role] as [string, ...string[]]),
+const userValidationSchema = z.object({
+    body: z.object({
+        password: z.string().max(20, {message: 'password can not be more then 20 characters'}),
+        role: z.enum([...Role] as [string, ...string[]]),
+    })
    
 });
 export const UserValidation = {
-    userVlidationSchema,
+    userValidationSchema,
 }
